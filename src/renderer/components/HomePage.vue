ea51aaa6c26a2a797834a3a21f00ade39312148d
TheCing <jptyndalljr@gmail.com>
2019-03-11 00:08:46 -0400

    init commit

---
+++ src/renderer/components/HomePage.vue
@@ -0,0 +1,92 @@
+<template>
+  <div id="home-page">
+    <nav-bar></nav-bar>
+    <div class="home-wrapper">
+      <!-- <div class="section">
+        <h1>Welcome</h1>
+      </div> -->
+      <div class="columns">
+        <div class="column is-narrow console-bar-wrapper">
+          <div class="console-bar">
+            <a class="console-link is-selected"><router-link to="/home"><img src="static/images/NES.png" alt="NES"></router-link></a>
+            <a class="console-link"><router-link to="/home"><img src="static/images/SNES.png" alt="SNES"></router-link></a>
+            <a class="console-link"><router-link to="/home"><img src="static/images/GB.png" alt="GB"></router-link></a>
+            <a class="console-link"><router-link to="/home"><img src="static/images/Genesis.png" alt="Genesis"></router-link></a>
+          </div>
+        </div>
+        <div class="column results-view-wrapper">
+          <div class="section results-view">
+            <h1 class="results-title">Nintendo Entertainment System</h1>
+            <hr>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import NavBar from './NavBar'
+
+  export default {
+    name: 'home',
+    components: { NavBar },
+    methods: {
+      open (link) {
+        this.$electron.shell.openExternal(link)
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  .home-wrapper {
+    color: white;
+
+    .console-bar-wrapper {
+      background-color: $color-light-gray;
+      padding-right: 0;
+      padding-top: 1.5em;
+    }
+  }
+
+  .columns {
+    height: -webkit-fill-available;
+  }
+
+  .console-bar {
+    padding: 1em 0.5em;
+
+    .console-link {
+      display: block;
+      img {
+        width: 4em;
+      }
+
+      &.is-selected {
+        img {
+          box-shadow: 0 0 5px white;
+          border-radius: 8px;
+        }
+      }
+    }
+  }
+
+  .results-view-wrapper {
+    background-color: $color-dark-gray;
+
+    .results-view {
+      .results-title {
+        text-transform: uppercase;
+        font-family: $font-body;
+        font-weight: 300;
+        font-size: 2em;
+        text-align: center;
+      }
+
+      hr {
+        margin: 0rem 4rem;
+      }
+    }
+  }
+</style>
---
+++ src/renderer/components/NavBar.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="nav-wrapper">
+    <nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <router-link class="navbar-item" to="/">
+          <h1 class="nav-title">Retrograde</h1>
+        </router-link>
+        <router-link class="navbar-item" to="/library" id="library">
+          <h1 class="nav-text">LIBRARY</h1>
+        </router-link>
+        <router-link class="navbar-item" to="/">
+          <h1 class="nav-text">PLAYLISTS</h1>
+        </router-link>
+        <router-link class="navbar-item" to="/">
+          <h1 class="nav-text">SAVES</h1>
+        </router-link>
+        <router-link class="navbar-item" to="/">
+          <h1 class="nav-text">CONFIG</h1>
+        </router-link>
+        <router-link class="navbar-item" to="/">
+          <h1 class="nav-text">SEARCH</h1>
+        </router-link>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        electron: process.versions.electron,
+        name: this.$route.name,
+        node: process.versions.node,
+        path: this.$route.path,
+        platform: require('os').platform(),
+        vue: require('vue/package.json').version
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  #nav-wrapper {
+    box-shadow: 0px 1px 10px 0px rgba(0, 0, 0, 0.31);
+  }
+
+  #library {
+    margin-left: 4em;
+  }
+
+  .navbar-brand {
+    .navbar-item {
+      font-size: 2em;
+
+      &:hover {
+        color: $color-indigo;
+      }
+    }
+  }
+
+  .nav-title {
+    font-family: $font-logo;
+  }
+
+  .nav-text {
+    font-family: $font-body;
+    // font-weight: 700;
+    font-style: italic;
+  }
+</style>
